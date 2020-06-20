d6f995ac51085981343a0da5c48d562d2a557412
Adam Kasztenny <adamkasztenny@gmail.com>
2020-06-20 19:15:13 -0400

    Simplify Name Remover

--- app/domain/name_remover.rb
+++ app/domain/name_remover.rb
@@ -3,21 +3,16 @@
 module Domain
   class NameRemover
     def remove(name, text_content)
-      without_name = remove_name(name, text_content)
-      remove_name_with_no_spaces(name, without_name)
+      name_fragments = name.split(/(?=[A-Z])/)
+      name_fragments.reduce(text_content) do |content, name_fragment|
+        remove_name(name_fragment, content)
+      end
     end
 
     private
 
     def remove_name(name, text_content)
       text_content.gsub(/#{name}/i, '')
-    end
-
-    def remove_name_with_no_spaces(name_without_spaces, text_content)
-      name_fragments = name_without_spaces.split(/(?=[A-Z])/)
-      name_fragments.reduce(text_content) do |content, name_fragment|
-        remove_name(name_fragment, content)
-      end
     end
   end
 end
