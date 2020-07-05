b91314e65533d13c95fe23ea8c3ab64fa1ba84b6
Adam Kasztenny <adamkasztenny@gmail.com>
2020-07-05 18:39:00 -0400

    Add ResumeUpload component to main App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import ResumeUpload from './ResumeUpload'
 
 function App() {
   return (
-    <h1>Resume Name Remover</h1>
+    <>
+      <h1>Resume Name Remover</h1>
+      <ResumeUpload data-testid="resume-upload" />
+    </>
   );
 }
 
