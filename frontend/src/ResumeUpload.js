a94b775fb08aeb56e3ab2052c51f97b664be7633
Adam Kasztenny <adamkasztenny@gmail.com>
2020-08-22 13:08:42 -0400

    Style ResumeUpload component

--- frontend/src/ResumeUpload.js
+++ frontend/src/ResumeUpload.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 function ResumeUpload() {
   return (
-    <form data-testid="upload-form" action="http://localhost:9000/remove" method="post" enctype="multipart/form-data">
-      <label htmlFor="data">Upload a resume in PDF format</label>
-      <input type="file" name="data" id="data" />
-      <input type="submit" />
+    <form data-testid="upload-form" action="http://localhost:9000/remove" method="post" encType="multipart/form-data">
+      <div className="form-group">
+        <label htmlFor="data">Upload a resume in PDF format</label>
+        <input type="file" name="data" id="data" className="form-control" />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
     </form>
   );
 }
