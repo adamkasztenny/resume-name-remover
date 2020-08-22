6251e73def022701e56897005482c43c3c11d007
Adam Kasztenny <adamkasztenny@gmail.com>
2020-08-22 12:45:23 -0400

    Styles main component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ResumeUpload from './ResumeUpload'
+import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>Resume Name Remover</h1>
-      <ResumeUpload data-testid="resume-upload" />
-    </>
+    <div className="container">
+      <div className="jumbotron">
+      	<h1 className="text-center">Resume Name Remover</h1>
+        <hr className="my-4" />
+      	<ResumeUpload data-testid="resume-upload" />
+      </div>
+    </div>
   );
 }
 
